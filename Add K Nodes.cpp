a2049f213eef8c5df4c25274941314ccea8ba433
Aditya Damare <aditya.damare.73@gmail.com>
2024-09-20 19:07:55 +0530

    Update Add K Nodes.cpp

--- Add K Nodes.cpp
+++ Add K Nodes.cpp
@@ -26,16 +26,16 @@ Node *getListAfterAddingNodes(Node *head, int k){
 
     while(temp){
         sum = sum + temp->data;
-        cnt++;
-        if(cnt == k){
+        count++;
+        if(count == k){
             Node* newNode = new Node(sum);
             newNode->next = temp->next;
             temp->next = newNode;
             temp = newNode;
             sum = 0;
-            cnt = 0;
+            count = 0;
         }
-        if(temp->next == NULL && cnt<k && cnt>0){
+        if(temp->next == NULL && count<k && count>0){
             Node* newNode = new Node(sum);
             newNode->next = temp->next;
             temp->next = newNode;
